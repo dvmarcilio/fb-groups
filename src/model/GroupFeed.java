f470dc652e9afa42d5eaea40e8c9f9336c71bd24
Diego Marcilio <dvmarcilio@gmail.com>
2016-04-05 23:11:49 -0300

    Parsing the entire group feed as List<Post>

---
+++ src/model/GroupFeed.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class GroupFeed {
+
+	public static void main(String[] args)
+			throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+				false);
+
+		InputStream is = GroupFeed.class
+				.getResourceAsStream("/data/maristao.json");
+		List<Post> posts = mapper.readValue(is, mapper.getTypeFactory()
+				.constructCollectionLikeType(List.class, Post.class));
+
+		Long totalInteractions = posts.stream()
+				.mapToLong(p -> p.getInteractions().size()).sum();
+		System.out.println("Total Interactions: " + totalInteractions);
+	}
+}
