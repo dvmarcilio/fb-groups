8d7364a3ad15f50980781aba55ddebdb645217c1
Diego Marcílio <dvmarcilio@gmail.com>
2016-04-07 16:53:56 -0300

    Testing that an interaction in Node increases the degrees correctly.

---
+++ test/model/graph/NodeTest.java
@@ -0,0 +1,41 @@
+package model.graph;
+
+import static org.junit.Assert.*;
+import model.fbdata.Interaction;
+import model.fbdata.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NodeTest {
+
+	private User u1;
+	private User u2;
+	private Node n1;
+	private Node n2;
+
+	@Before
+	public void setUp() {
+		u1 = new User(1L, "1");
+		n1 = new Node(u1);
+		u2 = new User(2L, "2");
+		n2 = new Node(u2);
+	}
+
+	@Test
+	public void interactingNodeOnlyIncreasesOutDegree() throws Exception {
+		n1.addInteractionWith(n2, Interaction.Type.MENTION);
+
+		assertEquals(0, n1.getInDegree());
+		assertEquals(1, n1.getOutDegree());
+	}
+
+	@Test
+	public void interactedNodeOnlyIncreasesInDegree() throws Exception {
+		n1.addInteractionWith(n2, Interaction.Type.MENTION);
+
+		assertEquals(1, n2.getInDegree());
+		assertEquals(0, n2.getOutDegree());
+	}
+
+}
