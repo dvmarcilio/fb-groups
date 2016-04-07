c8ca26fb447c6f70ac1cab4d156508ce39635432
Diego Marcílio <dvmarcilio@gmail.com>
2016-04-07 17:39:43 -0300

    Testing interactions incrementing total.
    * Pretty basic.

---
+++ test/model/graph/InteractionsTest.java
@@ -0,0 +1,39 @@
+package model.graph;
+
+import static org.junit.Assert.*;
+import model.fbdata.Interaction.Type;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InteractionsTest {
+
+	private Interactions interactions;
+
+	private int previousTotalComments;
+	private int previousTotalMentions;
+
+	@Before
+	public void setUp() {
+		interactions = new Interactions();
+		previousTotalComments = 0;
+		previousTotalMentions = 0;
+	}
+
+	@Test
+	public void addingCommentAddsTotalsCorrectly() throws Exception {
+		interactions.add(Type.COMMENT);
+		assertEquals(previousTotalMentions, interactions.getTotalMentions());
+		assertEquals(1, interactions.getTotalComments());
+		assertEquals(1, interactions.getTotal());
+	}
+
+	@Test
+	public void addingMentionAddsTotalsCorrectly() throws Exception {
+		interactions.add(Type.MENTION);
+		assertEquals(previousTotalComments, interactions.getTotalComments());
+		assertEquals(1, interactions.getTotalMentions());
+		assertEquals(1, interactions.getTotal());
+	}
+
+}
