c53f093e06456fbdb2fbaf450910e178ba4ba92e
Diego Marcilio <dvmarcilio@gmail.com>
2016-04-09 00:31:16 -0300

    Reading GroupFeed from new imported JSON, with more data.

--- src/model/fbdata/GroupFeed.java
+++ src/model/fbdata/GroupFeed.java
@@ -1,13 +1,7 @@
 package model.fbdata;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class GroupFeed {
 
 	private static final String JSON_EXTENSION = ".json";
+
 	@JsonProperty("data")
 	public List<Post> posts;
 
@@ -30,31 +25,30 @@ public class GroupFeed {
 
 	public static void main(String[] args)
 			throws JsonParseException, JsonMappingException, IOException {
+		List<Post> allPosts = retrieveAllPosts();
+
+		System.out.println("Total posts: " + allPosts.size());
+		Long totalInteractions = allPosts.stream()
+				.mapToLong(p -> p.getInteractions().size()).sum();
+		System.out.println("Total Interactions: " + totalInteractions);
+	}
+
+	public static List<Post> retrieveAllPosts()
+			throws IOException, JsonParseException, JsonMappingException {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
 				false);
 
-		// InputStream is = GroupFeed.class
-		// .getResourceAsStream("/data/maristao_novo_feed_2.json");
 		List<Post> allPosts = new LinkedList<>();
-		List<String> files = Arrays.asList("1", "2", "3");
+		List<String> files = Arrays.asList("1", "2", "3", "4", "5", "6", "7",
+				"8");
 
 		for (String fileName : files) {
 			GroupFeed feed = parseGroupFeedFromJson(mapper, fileName);
 			printFeedRead(fileName, feed);
 			allPosts.addAll(feed.getPosts());
 		}
-
-//		InputStream is = GroupFeed.class.getResourceAsStream("/data/3.json");
-//		takeBuggedCharOut(is);
-
-		// GroupFeed feed = mapper.readValue(json.substring(1),
-		// GroupFeed.class);
-
-		System.out.println("Total posts: " + allPosts.size());
-		Long totalInteractions = allPosts.stream()
-				.mapToLong(p -> p.getInteractions().size()).sum();
-		System.out.println("Total Interactions: " + totalInteractions);
+		return allPosts;
 	}
 
 	private static GroupFeed parseGroupFeedFromJson(ObjectMapper mapper,
@@ -73,23 +67,6 @@ public class GroupFeed {
 				.mapToLong(p -> p.getInteractions().size()).sum();
 		System.out.println("Interactions: " + totalInteractions);
 		System.out.println("================================================");
-	}
-
-	private static void takeBuggedCharOut(InputStream is) throws IOException,
-			UnsupportedEncodingException, FileNotFoundException {
-		String json = convertStreamToString(is);
-		json.replace("\r\n", "\\n");
-
-		try (Writer out = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream("teste.json"), "UTF-8"))) {
-			out.write(json);
-		}
-	}
-
-	static String convertStreamToString(java.io.InputStream is) {
-		java.util.Scanner s = new java.util.Scanner(is, "UTF-8")
-				.useDelimiter("\\A");
-		return s.hasNext() ? s.next() : "";
 	}
 
 }
